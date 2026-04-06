b9996ccc80f2ad29b1abf719ae2637ac48430bbc
sofi melesse <sofoniasmelesse@gmail.com>
2026-04-06 08:02:32 +0300

    3

--- next.config.js
+++ next.config.js
@@ -36,9 +36,9 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
 };
 
 module.exports = nextConfig;
