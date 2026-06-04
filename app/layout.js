f1cbdd33f16b379020ee41c12d51ca6efa8ae835
sofi melesse <sofoniasmelesse@gmail.com>
2026-06-04 06:39:34 +0300

    pixel_branch3

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,26 @@
+
+import { Providers as ThemeProvider } from '@/components/providers'
+import  Providers from '@/components/chat/chatproviders'
+import NewHeader from '@/components/layouts/new_header'
+import '@/styles/globals.css'
 import { FacebookPixel } from "./components";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        {children}
-        <FacebookPixel />
-      </body>
+      <Providers>
+        <body>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <div className='allparent'>
+              <NewHeader/>
+              <main className="flex-1">
+                {children}
+              </main>
+            </div>
+            <FacebookPixel />
+            </ThemeProvider>
+        </body>
+      </Providers>
     </html>
   );
 }
